244ed75156491144c0473113f024310166e3a5bd
manoflearning <66547669+manoflearning@users.noreply.github.com>
2020-07-04 19:29:05 +0900

    Update 3.1.1. Network Flow.cpp

--- Reference Codes/3. Maximum Flow/3.1. Network Flow/3.1.1. Network Flow.cpp
+++ Reference Codes/3. Maximum Flow/3.1. Network Flow/3.1.1. Network Flow.cpp
@@ -5,26 +5,11 @@
 using namespace std;
 
 const int MAXV = 1e3;
-const int INF = 1e9 + 7;
+const int INF = 1e9 + 7; 
 
-struct edge {
-	int v, c, f;
-	edge* dual; //역방향 간선을 가리키는 포인터
-	edge() : edge(-1, 0) {}
-	edge(int v1, int c1) : v(v1), c(c1), f(0), dual(nullptr) {}
-	int residual() {
-		return c - f;
-	}
-	void addFlow(int f1) {
-		f += f1;
-		dual->f -= f1;
-	}
-};
-
-int N;
-vector<edge*> adj[MAXV + 5];
+int N, c[MAXV + 5][MAXV + 5], f[MAXV + 5][MAXV + 5];
+vector<int> adj[MAXV + 5];
 int prv[MAXV + 5];
-edge* path[MAXV + 5];
 int st, en;
 
 void input();
@@ -33,74 +18,74 @@ void BFS(int st, int en);
 int flow(int st, int en);
 
 int main() {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-	input();
+    input();
 
-	st = 1, en = N;
-	int total = max_flow();
+    st = 1, en = N;
+    int total = max_flow();
 
-	cout << total << endl;
+    cout << total << endl;
 
-	return 0;
+    return 0;
 }
 
 void input() {
-	cin >> N;
+    cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		int n1, n2, cap;
-		cin >> n1 >> n2 >> cap;
+    for (int i = 0; i < N; i++) {
+        int n1, n2, cap;
+        cin >> n1 >> n2 >> cap;
 
-		edge* e1 = new edge(n2, cap), * e2 = new edge(n1, 0);
-		e1->dual = e2, e2->dual = e1;
-		adj[n1].push_back(e1);
-		adj[n2].push_back(e2);
-	}
+        c[n1][n2] += cap;
+
+        adj[n1].push_back(n2);
+        //역방향 간선을 추가해야 한다
+        adj[n2].push_back(n1);
+    }
 }
 
 int max_flow() {
-	int ret = 0;
-	while (true) {
-		BFS(st, en);
+    int ret = 0;
+    while (true) {
+        BFS(st, en);
 
-		if (prv[en] == -1) break;
+        if (prv[en] == -1) break;
 
-		ret += flow(st, en);
-	}
-	return ret;
+        ret += flow(st, en);
+    }
+    return ret;
 }
 
 void BFS(int st, int en) {
-	memset(prv, -1, sizeof(prv));
+    memset(prv, -1, sizeof(prv));
 
-	queue<int> q;
-	q.push(st);
-	prv[st] = 0;
+    queue<int> q;
+    q.push(st);
+    prv[st] = 0;
 
-	while (!q.empty() && prv[en] == -1) {
-		int now = q.front();
-		q.pop();
+    while (!q.empty() && prv[next] == -1) {
+        int now = q.front();
+        q.pop();
 
-		for (auto* e : adj[now]) {
-			int next = e->v;
-			if (prv[next] == -1 && e->residual() > 0) {
-				q.push(next);
-				prv[next] = now;
-				path[next] = e;
-			}
-		}
-	}
+        for (int next : adj[now]) {
+            if (prv[next] == -1 && c[now][next] - f[now][next] > 0) {
+                q.push(next);
+                prv[next] = now;
+            }
+        }
+    }
 }
 
 int flow(int st, int en) {
-	int n = en, block = INF;
-	for (int i = en; i != st; i = prv[i]) {
-		block = min(block, path[i]->residual());
-	}
-	for (int i = en; i != st; i = prv[i]) {
-		path[i]->addFlow(block);
-	}
-	return block;
+    int block = INF;
+    for (int i = en; i != st; i = prv[i]) {
+        block = min(block, c[prv[i]][i] - f[prv[i]][i]);
+    }
+    for (int i = en; i != st; i = prv[i]) {
+        f[prv[i]][i] += block;
+        f[i][prv[i]] -= block;
+    }
+    return block;
 }
