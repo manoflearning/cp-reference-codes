b6cfa6ab765e9cc329ef488b0c95ff0ed04fd5f7
manoflearning <66547669+manoflearning@users.noreply.github.com>
2020-07-15 23:25:17 +0900

    Change Segment Tree into a struct

--- Reference Codes/1. Data Structure/1.1. Segment Tree/1.1.1. Segment Tree.cpp
+++ Reference Codes/1. Data Structure/1.1. Segment Tree/1.1.1. Segment Tree.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <vector>
-#define ll long long
 using namespace std;
+#define ll long long
 
-int flag;  // array size
-vector<ll> t;
+struct Seg {
+	int flag;  // array size
+	vector<ll> t;
 
-void modify(int p, int value);
-ll query(int L, int R, int n, int nL, int nR);
+	void build(int N) {
+		for (flag = 1; flag < N; flag <<= 1);
+		t.resize(2 * flag);
+
+		for (int i = 0; i < N; i++) cin >> t[flag + i];
+		for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
+	void modify(int p, int value) {  // set value at position p
+		for (t[p += flag] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+	}
+	ll query(int L, int R) {
+		return query(L, R, 1, 0, flag - 1);  // index starts at 0
+	}
+	ll query(int L, int R, int n, int nL, int nR) {  // sum on interval [L, R]
+		if (R < nL || nR < L) return 0;
+		if (L <= nL && nR <= R) return t[n];
+
+		int mid = (nL + nR) / 2;
+		return query(L, R, n << 1, nL, mid) + query(L, R, n << 1 | 1, mid + 1, nR);
+	}
+}seg;
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
@@ -15,26 +35,10 @@ int main() {
 
 	int N; cin >> N;
 
-	for (flag = 1; flag < N; flag <<= 1);
-	t.resize(2 * flag);
+	seg.build(N);
 
-	for (int i = 0; i < N; i++) cin >> t[flag + i];
-	for (int i = flag - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
-
-	modify(0, 1);
-	cout << query(3, 11, 1, 0, flag - 1) << '\n';
+	seg.modify(0, 1);
+	cout << seg.query(3, 11) << '\n';
 
 	return 0;
-}
-
-void modify(int p, int value) {  // set value at position p
-	for (t[p += flag] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
-}
-
-ll query(int L, int R, int n, int nL, int nR) {  // sum on interval [L, R]
-	if (R < nL || nR < L) return 0;
-	if (L <= nL && nR <= R) return t[n];
-
-	int mid = (nL + nR) / 2;
-	return query(L, R, n << 1, nL, mid) + query(L, R, n << 1 | 1, mid + 1, nR);
 }
--- Reference Codes/1. Data Structure/1.1. Segment Tree/1.1.3. Iterative Segment Tree.cpp
+++ Reference Codes/1. Data Structure/1.1. Segment Tree/1.1.3. Iterative Segment Tree.cpp
@@ -4,36 +4,37 @@ using namespace std;
 
 const int MAXN = 1e5;  // limit for array size
 
-int N;  // array size
-ll t[2 * MAXN];
+struct Seg {
+	int N;  // array size
+	ll t[2 * MAXN];
 
-void modify(int p, int value);
-ll query(int L, int R);
+	void build(int N) {
+		for (int i = 0; i < N; i++) cin >> t[N + i];
+		for (int i = N - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
+	void modify(int p, int value) {  // set value at position p
+		for (t[p += N] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+	}
+	ll query(int L, int R) {  // sum on interval [L, R)
+		ll ret = 0;
+		for (L += N, R += N; L < R; L >>= 1, R >>= 1) {
+			if (L & 1) ret += t[L++];
+			if (R & 1) ret += t[--R];
+		}
+		return ret;
+	}
+}seg;
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+	
+	int N; cin >> N;
 
-	cin >> N;
+	seg.build(N);
 
-	for (int i = 0; i < N; i++) cin >> t[N + i];
-	for (int i = N - 1; i >= 1; i--) t[i] = t[i << 1] + t[i << 1 | 1];
-
-	modify(0, 1);
-	cout << query(3, 11);
+	seg.modify(0, 1);
+	cout << seg.query(3, 11);
 
 	return 0;
-}
-
-void modify(int p, int value) {  // set value at position p
-	for (t[p += N] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
-}
-
-ll query(int L, int R) {  // sum on interval [L, R)
-	ll ret = 0;
-	for (L += N, R += N; L < R; L >>= 1, R >>= 1) {
-		if (L & 1) ret += t[L++];
-		if (R & 1) ret += t[--R];
-	}
-	return ret;
 }
