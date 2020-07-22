7a332bdde6c0ebd9abe496c65fbc99429bea169c
manoflearning <66547669+manoflearning@users.noreply.github.com>
2020-07-22 19:07:20 +0900

    Update 대회용 기본 템플릿.cpp

--- 대회용 기본 템플릿.cpp
+++ 대회용 기본 템플릿.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -8,7 +9,6 @@ const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1 };
 const int dx[] = { 1, -1, 0, 0 };
-const long double PI = 3.141592653589793238462643383279502884197;
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
