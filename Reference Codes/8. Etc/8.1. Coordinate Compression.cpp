6b7ad23015458dae7d0759afd59d67fd5f943f15
manoflearning <77jwk0724@gmail.com>
2022-02-06 20:30:28 +0900

    Create 8.1. Coordinate Compression.cpp

---
+++ Reference Codes/8. Etc/8.1. Coordinate Compression.cpp
@@ -0,0 +1,18 @@
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	vector<int> a;
+
+	sort(a.begin(), a.end());
+	a.erase(unique(a.begin(), a.end()), a.end());
+
+	int q; cin >> q;
+	for (int i = 0; i < q; i++) {
+		int num; cin >> num;
+		
+		cout << lower_bound(a.begin(), a.end(), num) - a.begin();
+	}
+
+	return 0;
+}
