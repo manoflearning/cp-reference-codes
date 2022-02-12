dd97379b5623846c8e8574aebe3992b95bc65a30
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-02-12 19:10:29 +0900

    Create 1.4. Sqrt Decomposition, Mo's Algorithm.cpp

---
+++ Reference Codes/1. Data Structure/1.4. Sqrt Decomposition, Mo's Algorithm.cpp
@@ -0,0 +1,58 @@
+int sq;
+
+struct se {
+	int s, e, idx;
+	bool operator<(se a) {
+		if (s / sq != a.s / sq) return s / sq < a.s / sq;
+		return e < a.e;
+	}
+};
+
+vector<se> q;
+vector<int> ans;
+
+void input() {
+	// TODO: receive input | resize q, ans | calculate sq 
+}
+
+void add(int idx) {
+	// TODO: add value at idx from data structure
+}
+
+void del(int idx) {
+	// TODO: remove value at idx from data structure
+}
+
+int query() {
+	// TODO: extract the current answer of the data structure
+}
+
+void f() {
+	int s = q[0].s, e = q[0].e;
+	// TODO: initialize data structure
+	ans[q[0].idx] = query();
+
+	for (int i = 1; i < q.size(); i++) {
+		while (q[i].s < s) add(--s);
+		while (e < q[i].e) add(++e);
+		while (s < q[i].s) del(s++);
+		while (q[i].e < e) del(e--);
+		ans[q[i].idx] = query();
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	input();
+
+	sort(q.begin(), q.end());
+
+	f();
+
+	for (auto& i : ans) 
+		cout << i << '\n';
+
+	return 0;
+}
