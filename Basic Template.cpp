17748472b1b1baff44cb3448a342be42ddb95ef9
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-02-13 20:45:18 +0900

    Update Basic Template.cpp

--- Basic Template.cpp
+++ Basic Template.cpp
@@ -13,14 +13,15 @@ using namespace std;
 #define all(c) (c).begin(), (c).end()
 #define sz(x) (int)(x).size()
 
+#define EXPAND( x ) x // Use this if Microsoft Visual Studio doesn't expand __VA_ARGS__ correctly
 #define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
 #define F_OR1(e) F_OR(i, 0, e, 1)
 #define F_OR2(i, e) F_OR(i, 0, e, 1)
 #define F_OR3(i, b, e) F_OR(i, b, e, 1)
 #define F_OR4(i, b, e, s) F_OR(i, b, e, s)
 #define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
-#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
 #define EACH(x, a) for (auto& x : a)
 
 const int INF = 1e9 + 7;
@@ -28,8 +29,12 @@ const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
-void solve() {
+const int MAX = 1e3 + 5;
 
+int n, a[MAX][MAX];
+
+void solve() {
+	
 }
 
 int main() {
