426e926d8b9a272e829d5f168343b8d3df711fc9
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-02-13 17:42:46 +0900

    Update Basic Template.cpp

--- Basic Template.cpp
+++ Basic Template.cpp
@@ -3,25 +3,40 @@
 #include <cassert>
 using namespace std;
 #define ll long long
+#define ld long double
 #define pii pair<int, int>
 #define pll pair<ll, ll>
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define vl vector<ll>
-#define vvl vector<vector<ll>>
-#define vs vector<string>
-#define vvs vector<vector<string>>
 #define fr first
 #define sc second
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
+#define EACH(x, a) for (auto& x : a)
 
 const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
 const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
+void solve() {
+
+}
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	
+
+	solve();
+
 	return 0;
 }
