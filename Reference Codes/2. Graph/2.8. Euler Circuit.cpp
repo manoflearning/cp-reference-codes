9f6ada8481a1a6d376915afe9ae1a43e234a7484
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-17 14:48:44 +0900

    .

--- Reference Codes/2. Graph/2.8. Euler Circuit.cpp
+++ Reference Codes/2. Graph/2.8. Euler Circuit.cpp
@@ -1,3 +1,4 @@
+// Hierholzer's Algorithm
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,13 +9,11 @@ int N, adj[MAXV + 5][MAXV + 5];
 vector<int> euler_circult;
 
 void dfs(int now) {
-	//인접행렬
 	for(int next = 1; next <= N; next++)
 		if (adj[now][next]) {
 			adj[now][next]--;
 			adj[next][now]--;
 			dfs(next);
 		}
-	//경로의 역순으로 배열이 구성된다.
 	euler_circult.push_back(now);
 }
