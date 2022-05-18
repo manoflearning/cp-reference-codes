3f04039d6204846eed96b3ffa0cc7519c869cea8
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-18 11:45:53 +0900

    .

--- Reference Codes/2. Graph/2.11. BCC.cpp
+++ Reference Codes/2. Graph/2.11. BCC.cpp
@@ -1,39 +1,43 @@
-int N, dfsn[MAXV + 5], dCnt;
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int MAXV = 101010;
+
+int n, m, dfsn[MAXV + 5], dCnt;
 vector<int> adj[MAXV + 5];
 stack<pair<int, int>> stk;
 vector<vector<pair<int, int>>> bcc;
 
-int dfs(int now, int prv = -1);
-
-int main() {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
-	memset(dfsn, -1, sizeof(dfsn));
-
-	for (int v = 1; v <= N; v++)
-		if (dfsn[v] == -1) dfs(v);
-
-	return 0;
+void input() {
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	} 
 }
 
 int dfs(int now, int prv) {
 	int ret = dfsn[now] = ++dCnt;
 
 	for (int next : adj[now]) {
-		//�θ� ��� ��ŵ
 		if (next == prv) continue;
 
-		//���� �湮���� ���� 
+		// If an edge { now, next } has not yet been visited, it puts an edge on the stack.
 		if (dfsn[now] > dfsn[next]) stk.push({ now, next });
-		//������ ����
+		// Back edge
 		if (dfsn[next] != -1) ret = min(ret, dfsn[next]);
-		//Ʈ�� ����
+		// Tree edge
 		else {
 			int tmp = dfs(next, now);
 			ret = min(ret, tmp);
             
-			//�ڼ� ��忡�� ���� ����� ���� ���� �� �� �ִ� ��ΰ� ����
+			// if next cannot go to ancestor node of now, find BCC
 			if (tmp >= dfsn[now]) {
 				vector<pair<int, int>> nowBCC;
 				while (true) {
@@ -51,4 +55,22 @@ int dfs(int now, int prv) {
 	}
 
 	return ret;
+}
+
+void getBCC() {
+	memset(dfsn, -1, sizeof(dfsn));
+
+	for (int v = 1; v <= n; v++)
+		if (dfsn[v] == -1) dfs(v, 0);
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	input();
+
+	getBCC();
+
+	return 0;
 }
