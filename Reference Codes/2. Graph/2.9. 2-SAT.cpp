14f90d8b51fb207046458d24e8026b6cd339a5fc
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-05-24 21:20:00 +0900

    .

--- Reference Codes/2. Graph/2.9. 2-SAT.cpp
+++ Reference Codes/2. Graph/2.9. 2-SAT.cpp
@@ -5,13 +5,19 @@
 #include <stack>
 #include <algorithm>
 using namespace std;
+#define pii pair<int, int>
+#define fr first
+#define sc second
 
-const int MAXV = 2020;
-
+const int MAXV = 20202;
+	
+int n, m;
 int dfsn[MAXV], dCnt, sNum[MAXV], sCnt;
-bool finished[MAXV];
+int finished[MAXV];
 vector<int> adj[MAXV];
 stack<int> stk;
+pii p[MAXV];
+int ans[MAXV / 2];
 
 inline int trans(int x) {
 	return (x > 0) ? 2 * (x - 1) : 2 * (-x - 1) + 1;
@@ -28,12 +34,12 @@ int dfs(int now) {
 	}
 
 	if (ret >= dfsn[now]) {
-		while (true) {
+		while (1) {
 			int t = stk.top();
 			stk.pop();
 
 			sNum[t] = sCnt;
-			finished[t] = true;
+			finished[t] = 1;
 			
 			if (t == now) break;
 		}
@@ -43,27 +49,7 @@ int dfs(int now) {
 	return ret;
 }
 
-int main() {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
-	memset(dfsn, -1, sizeof(dfsn));
-
-	int n, m;
-	cin >> n >> m;
-
-	// graph modeling
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		adj[trans(-a)].push_back(trans(b));
-		adj[trans(-b)].push_back(trans(a));
-	}
-
-	// scc (dfs tree)
-	for (int v = 0; v < 2 * n; v++)
-		if (dfsn[v] == -1) dfs(v);
-
+int isSatisfiable() {
 	// determining satisfiability 
 	int isS = 1;
 	for (int v = 0; v < 2 * n; v += 2) {
@@ -73,9 +59,55 @@ int main() {
 			break;
 		}
 	}
+	return isS;
+}
 
-	if (isS) cout << 1;
-	else cout << 0;
+void findValueOfEachVariable() {
+	// order of scc is the reverse of the topological sort
+	for (int v = 0; v < 2 * n; v++) {
+		p[v] = { sNum[v], v };
+	}
+
+	sort(p, p + 2 * n);
+
+	// determining true/false of each variable
+	for (int i = 2 * n - 1; i >= 0; i--) {
+		int v = p[i].sc;
+		if (ans[v / 2 + 1] == -1) 
+			ans[v / 2 + 1] = (v & 1) ? 1 : 0;
+	}
+		
+	for (int v = 1; v <= n; v++)
+		cout << ans[v] << ' ';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	memset(dfsn, -1, sizeof(dfsn));
+	memset(ans, -1, sizeof(ans));
+
+	cin >> n >> m;
+
+	// graph modeling
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		// negative number -a indicates the negation of the variable a.
+		adj[trans(-a)].push_back(trans(b));
+		adj[trans(-b)].push_back(trans(a));
+	}
+
+	// finding scc
+	for (int v = 0; v < 2 * n; v++)
+		if (dfsn[v] == -1) dfs(v);
+
+	if (isSatisfiable()) {
+		cout << "is satisfiable" << '\n';
+		findValueOfEachVariable();
+	}
+	else cout << "is not satisfiable";
 
 	return 0;
 }
