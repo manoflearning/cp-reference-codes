318398a10e4aaeca9d7bffb31e9a6336565bfcc4
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-27 19:49:06 +0900

    change KMP

--- Reference Codes/5. String/5.2. KMP Algorithm.cpp
+++ Reference Codes/5. String/5.2. KMP Algorithm.cpp
@@ -1,56 +1,16 @@
+// BOJ 1786 AC Code
+// https://www.acmicpc.net/problem/1786
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
-
-vector<int> kmp(const string& T, const string& P);
-vector<int> getpi(const string& P);
-
-int main() {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
-	string T, P;
-	getline(cin, T);
-	getline(cin, P);
-
-	vector<int> ans = kmp(T, P);
-
-	cout << ans.size() << '\n';
-	for (int i : ans)
-		cout << i + 1 << '\n';
-
-	return 0;
-}
-
-vector<int> kmp(const string& T, const string& P) {
-	vector<int> ret;
-
-	vector<int> pi = getpi(P);
-
-	int begin = 0, matched = 0;
-	while (begin <= T.size() - P.size()) {
-		if (matched < P.size() && T[begin + matched] == P[matched]) {
-			matched++;
-			if (matched == P.size()) ret.push_back(begin);
-		}
-		else {
-			if (matched == 0) begin++;
-			else {
-				begin += matched - pi[matched - 1];
-				matched = pi[matched - 1];
-			}
-		}
-	}
-
-	return ret;
-}
+#define sz(x) (int)(x).size()
 
 vector<int> getpi(const string& P) {
-	vector<int> pi(P.size());
+	vector<int> pi(sz(P));
 
 	int begin = 1, matched = 0;
-	while (begin + matched < P.size()) {
+	while (begin + matched < sz(P)) {
 		if (P[begin + matched] == P[matched]) {
 			matched++;
 			pi[begin + matched - 1] = matched;
@@ -65,4 +25,44 @@ vector<int> getpi(const string& P) {
 	}
 
 	return pi;
+}
+
+vector<int> kmp(const string& T, const string& P) {
+	vector<int> ret;
+
+	vector<int> pi = getpi(P);
+
+	int begin = 0, matched = 0;
+	while (begin <= sz(T) - sz(P)) {
+		if (matched < sz(P) && T[begin + matched] == P[matched]) {
+			matched++;
+			if (matched == sz(P)) ret.push_back(begin);
+		}
+		else {
+			if (matched == 0) begin++;
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
+
+	return ret;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string T, P;
+	getline(cin, T);
+	getline(cin, P);
+
+	vector<int> ans = kmp(T, P);
+
+	cout << sz(ans) << '\n';
+	for (int i : ans)
+		cout << i + 1 << '\n';
+
+	return 0;
 }
