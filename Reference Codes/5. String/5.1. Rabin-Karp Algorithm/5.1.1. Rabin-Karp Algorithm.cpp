612a15a5685fd7fdb9012f9b0e812e31ef8da9ca
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-06-27 20:06:02 +0900

    change Rabin-Karp algorithm

--- Reference Codes/5. String/5.1. Rabin-Karp Algorithm/5.1.1. Rabin-Karp Algorithm.cpp
+++ Reference Codes/5. String/5.1. Rabin-Karp Algorithm/5.1.1. Rabin-Karp Algorithm.cpp
@@ -1,18 +1,44 @@
+// BOJ 1786 TLE Code
+// https://www.acmicpc.net/problem/1786
+// The basic Rabin-Karp Algorithm works on average in O(N + M), but in the worst case it works on O(NM)
 #include <iostream>
 #include <string>
 #include <vector>
 #define ll long long
 using namespace std;
+#define sz(x) (int)(x).size()
 
-const int MAX = 1e6 + 5;
-const ll MOD = 1e9 + 7;
+const int MAX = 1010101;
+const int MOD = 1e9 + 7;
 
 string T, P;
 ll d = 128, dexp[MAX];
 vector<int> ans;
 
-void rabinKarp();
-bool match(int s);
+bool match(int s) {
+	for (int i = 0; i < sz(P); i++) {
+		if (P[i] != T[s + i]) return false;
+	}
+	return true;
+}
+
+void rabinKarp() {
+	int len = sz(P);
+
+	ll p = 0, t = 0;
+	for (int i = 0; i < len; i++) {
+		p = (d * p + P[i]) % MOD;
+		t = (d * t + T[i]) % MOD;
+	}
+
+	if (p == t && match(0)) ans.push_back(0);
+
+	for (int i = 1; i < sz(T) - len + 1; i++) {
+		t = (d * (t - dexp[len - 1] * T[i - 1]) + T[i + len - 1]) % MOD;
+		t = (t + MOD) % MOD;
+		if (p == t && match(i)) ans.push_back(i);
+	}
+}
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
@@ -27,33 +53,8 @@ int main() {
 
 	rabinKarp();
 
-	cout << ans.size() << '\n';
-	for (int i : ans) cout << i << ' ';
+	cout << sz(ans) << '\n';
+	for (int i : ans) cout << i + 1 << ' ';
 
 	return 0;
-}
-
-void rabinKarp() {
-	int len = P.size();
-
-	ll p = 0, t = 0;
-	for (int i = 0; i < len; i++) {
-		p = (d * p + P[i]) % MOD;
-		t = (d * t + T[i]) % MOD;
-	}
-
-	if (p == t && match(0)) ans.push_back(0);
-
-	for (int i = 1; i < T.size() - len + 1; i++) {
-		t = (d * (t - dexp[len - 1] * T[i - 1]) + T[i + len - 1]) % MOD;
-		t = (t + MOD) % MOD;
-		if (p == t && match(i)) ans.push_back(i);
-	}
-}
-
-bool match(int s) {
-	for (int i = 0; i < P.size(); i++) {
-		if (P[i] != T[s + i]) return false;
-	}
-	return true;
 }
--- Reference Codes/5. String/5.1. Rabin-Karp Algorithm/5.1.2. Rabin-Karp Algortihm - Double Hashing.cpp
+++ Reference Codes/5. String/5.1. Rabin-Karp Algorithm/5.1.2. Rabin-Karp Algortihm - Double Hashing.cpp
@@ -1,17 +1,40 @@
+// BOJ 1786 AC Code
+// https://www.acmicpc.net/problem/1786
 #include <iostream>
 #include <string>
 #include <vector>
 #define ll long long
 using namespace std;
+#define sz(x) (int)(x).size()
 
-const int MAX = 1e6 + 5;
-const ll MOD1 = 1e9 + 7, MOD2 = 1e9 + 9;
+const int MAX = 1010101;
+const int MOD1 = 1e9 + 7, MOD2 = 1e9 + 9;
 
 string T, P;
 ll d = 128, dexp1[MAX], dexp2[MAX];
 vector<int> ans;
 
-void rabinKarp();
+void rabinKarp() {
+	int len = sz(P);
+
+	ll p1 = 0, p2 = 0, t1 = 0, t2 = 0;
+	for (int i = 0; i < len; i++) {
+		p1 = (d * p1 + P[i]) % MOD1;
+		p2 = (d * p2 + P[i]) % MOD2;
+		t1 = (d * t1 + T[i]) % MOD1;
+		t2 = (d * t2 + T[i]) % MOD2;
+	}
+
+	if (p1 == t1 && p2 == t2) ans.push_back(0);
+
+	for (int i = 1; i < sz(T) - len + 1; i++) {
+		t1 = (d * (t1 - dexp1[len - 1] * T[i - 1]) + T[i + len - 1]) % MOD1;
+		t1 = (t1 + MOD1) % MOD1;
+		t2 = (d * (t2 - dexp2[len - 1] * T[i - 1]) + T[i + len - 1]) % MOD2;
+		t2 = (t2 + MOD2) % MOD2;
+		if (p1 == t1 && p2 == t2) ans.push_back(i);
+	}
+}
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
@@ -28,30 +51,8 @@ int main() {
 
 	rabinKarp();
 
-	cout << ans.size() << '\n';
-	for (int i : ans) cout << i << ' ';
+	cout << sz(ans) << '\n';
+	for (int i : ans) cout << i + 1 << ' ';
 
 	return 0;
-}
-
-void rabinKarp() {
-	int len = P.size();
-
-	ll p1 = 0, p2 = 0, t1 = 0, t2 = 0;
-	for (int i = 0; i < len; i++) {
-		p1 = (d * p1 + P[i]) % MOD1;
-		p2 = (d * p2 + P[i]) % MOD2;
-		t1 = (d * t1 + T[i]) % MOD1;
-		t2 = (d * t2 + T[i]) % MOD2;
-	}
-
-	if (p1 == t1 && p2 == t2) ans.push_back(0);
-
-	for (int i = 1; i < T.size() - len + 1; i++) {
-		t1 = (d * (t1 - dexp1[len - 1] * T[i - 1]) + T[i + len - 1]) % MOD1;
-		t1 = (t1 + MOD1) % MOD1;
-		t2 = (d * (t2 - dexp2[len - 1] * T[i - 1]) + T[i + len - 1]) % MOD2;
-		t2 = (t2 + MOD2) % MOD2;
-		if (p1 == t1 && p2 == t2) ans.push_back(i);
-	}
 }
