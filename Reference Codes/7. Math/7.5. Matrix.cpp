1b618a0a49eb9609c9f421f044eca1dd79e544b7
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-10 20:54:34 +0900

    Add Matrix

---
+++ Reference Codes/7. Math/7.5. Matrix.cpp
@@ -0,0 +1,22 @@
+// Unverified code, many features to be added
+#define sz(x) (int)(x).size()
+
+const int MOD = 1e9 + 7;
+
+struct Matrix {
+	vt<vt<ll>> a;
+	Matrix operator*(const Matrix& rhs) const {
+		Matrix ret;
+		ret.a.resize(sz(a), vt<ll>(sz(rhs.a[0])));
+		for (int y = 0; y < sz(ret.a); y++) {
+			for (int x = 0; x < sz(ret.a[y]); x++) {
+				ll sum = 0;
+				for (int i = 0; i < sz(a[y]); i++) {
+					sum = (sum + a[y][i] * rhs.a[i][x]) % MOD;
+				}
+				ret.a[y][x] = sum;
+			}
+		}
+		return ret;
+	}
+};
