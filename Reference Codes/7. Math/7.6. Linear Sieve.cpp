17abc1d4f6b04d1b33b64b8156bdd46f1a69e753
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-21 23:22:31 +0900

    Add Linear Sieve

---
+++ Reference Codes/7. Math/7.6. Linear Sieve.cpp
@@ -0,0 +1,49 @@
+// BOJ 16563 AC Code
+// https://www.acmicpc.net/problem/16563
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int MAXN = 5000000;
+
+vector<int> sp(MAXN + 1);
+vector<ll> prime;
+
+// Determine prime numbers between 1 and MAXN in O(MAXN) 
+void linearSieve() {
+	for (int i = 2;i <= MAXN; i++) {
+		if (!sp[i]) {
+			prime.push_back(i);
+			sp[i] = i;
+		}
+		for (auto j : prime) {
+			if (i * j > MAXN) break;
+			sp[i * j] = j;
+			if (i % j == 0) break;
+		}
+	}
+}
+
+// factorization in O(log x)
+void factorization(int x) {
+    while (x > 1) {
+		cout << sp[x] << ' ';
+		x /= sp[x];
+	}
+	cout << '\n';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	linearSieve();
+
+	int n; cin >> n;
+	while (n--) {
+		int x; cin >> x;
+		factorization(x);
+	}
+
+	return 0;
+}
