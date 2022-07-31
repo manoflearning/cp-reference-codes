9ff7084b251ed01519184618a65241ea3fdb83a0
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-31 10:54:13 +0900

    .

--- Reference Codes/8. Etc/8.1. Coordinate Compression.cpp
+++ Reference Codes/8. Etc/8.1. Coordinate Compression.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
--- Reference Codes/8. Etc/8.2. 2D Prefix Sum.cpp
+++ Reference Codes/8. Etc/8.2. 2D Prefix Sum.cpp
@@ -2,13 +2,13 @@ const int MAX = 1010;
 
 ll arr[MAX][MAX], psum[MAX][MAX];
 
-void makingPsum() {
-	FOR(i, 1, MAX) {
-		FOR(j, 1, MAX) {
-            psum[i][j] += arr[i][j];
-            psum[i][j] += psum[i][j - 1];
-            psum[i][j] += psum[i - 1][j];
-            psum[i][j] -= psum[i - 1][j - 1];
-		}
-	}
+void buildPsum() {
+      FOR(i, 1, MAX) {
+            FOR(j, 1, MAX) {
+                  psum[i][j] += arr[i][j];
+                  psum[i][j] += psum[i][j - 1];
+                  psum[i][j] += psum[i - 1][j];
+                  psum[i][j] -= psum[i - 1][j - 1];
+            }
+      }
 }
