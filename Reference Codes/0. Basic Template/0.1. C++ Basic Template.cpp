81dbdd43f0b605946a647665956e29cc1eb821dc
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-07-31 09:36:45 +0900

    Change Basic Template

--- Reference Codes/0. Basic Template/0.1. C++ Basic Template.cpp
+++ Reference Codes/0. Basic Template/0.1. C++ Basic Template.cpp
@@ -34,14 +34,17 @@ int main() {
 	#ifndef ONLINE_JUDGE
 	// Enter the absolute path of the local file input.txt, output.txt
 	// Or just enter the "input.txt", "output.txt"
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
 	#endif
 
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	
+	int tc; cin >> tc;
+	FOR(tt, 1, tc + 1) {
+		
+	}
 
 	return 0;
 }
