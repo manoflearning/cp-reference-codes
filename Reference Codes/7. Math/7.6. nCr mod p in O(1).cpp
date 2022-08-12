2a67b540530f378680daab4c9f938bc62cfa7405
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-08-12 22:48:02 +0900

    Change nCr mod p in O(1)

--- Reference Codes/7. Math/7.6. nCr mod p in O(1).cpp
+++ Reference Codes/7. Math/7.6. nCr mod p in O(1).cpp
@@ -14,18 +14,18 @@ ll binom(int n, int r) {
 }
 
 int main() {
-    cin.tie(NULL); cout.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
 	// Preprocessing in O(N)
 	fac[0] = fac[1] = inv[1] = 1;
-    facInv[0] = facInv[1] = 1;
+	facInv[0] = facInv[1] = 1;
 	for (int i = 2; i < MAXN; i++) {
 		fac[i] = i * fac[i - 1] % MOD;
 
 		inv[i] = -(MOD / i) * inv[MOD % i] % MOD;
-        if (inv[i] < 0) inv[i] += MOD;
-        facInv[i] = facInv[i - 1] * inv[i] % MOD;
+		if (inv[i] < 0) inv[i] += MOD;
+		facInv[i] = facInv[i - 1] * inv[i] % MOD;
 	}
 
 	// Answer each query in O(1)
