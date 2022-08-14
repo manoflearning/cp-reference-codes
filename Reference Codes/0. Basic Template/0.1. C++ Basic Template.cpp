1ef0df4d063cce06afe4884115148fc242066b99
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-08-15 08:29:34 +0900

    Change C++ Basic Template.cpp

--- Reference Codes/0. Basic Template/0.1. C++ Basic Template.cpp
+++ Reference Codes/0. Basic Template/0.1. C++ Basic Template.cpp
@@ -32,8 +32,6 @@ const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
 int main() {
 	#ifndef ONLINE_JUDGE
-	// Enter the absolute path of the local file input.txt, output.txt
-	// Or just enter the "input.txt", "output.txt"
 	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
 	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
 	#endif
