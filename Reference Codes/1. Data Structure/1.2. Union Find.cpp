feda7592cba053901d5dd5dbb868cffa4e4d742e
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-11-04 20:08:37 +0900

    Change Union Find

--- Reference Codes/1. Data Structure/1.2. Union Find.cpp
+++ Reference Codes/1. Data Structure/1.2. Union Find.cpp
@@ -6,18 +6,18 @@ using namespace std;
 const int MAX = 1010101;
 
 int n;
-vector<int> p(MAX, -1);
+vector<int> uf(MAX, -1);
 
 int find(int x) {
-	if (p[x] < 0) return x;
-	return p[x] = find(p[x]);
+	if (uf[x] < 0) return x;
+	return uf[x] = find(uf[x]);
 }
 
 void merge(int a, int b) {
 	int A = find(a), B = find(b);
 	if (A == B) return;
-	p[A] += p[B];
-	p[B] = A;
+	uf[A] += uf[B];
+	uf[B] = A;
 }
 
 int main() {
