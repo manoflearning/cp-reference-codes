c25d9775eb6fedeffac508b3c259fce6928c221f
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-11-08 23:46:29 +0900

    Change FFT

--- Reference Codes/7. Math/7.11. FFT.cpp
+++ Reference Codes/7. Math/7.11. FFT.cpp
@@ -26,7 +26,7 @@ void fft(vector<base> &a, bool inv) {
 			}
 		}
 	}
-	if (inv) for (int i = 0; i < n; i++) a[i] /= n; // skip for OR convolution.
+	if (inv) for (int i = 0; i < n; i++) a[i] /= n;
 }
 
 void multiply(const vector<ll> &v, const vector<ll> &w, vector<ll>& res) {
