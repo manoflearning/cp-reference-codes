8ba249cf1fc3f1416838e6a8e5d94744a309397f
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-12-20 04:54:43 +0900

    Change C++ Basic Template.cpp

--- Reference Codes/0. Basic Template/0.1. C++ Basic Template.cpp
+++ Reference Codes/0. Basic Template/0.1. C++ Basic Template.cpp
@@ -12,17 +12,6 @@ using namespace std;
 #define all(c) (c).begin(), (c).end()
 #define sz(x) (int)(x).size()
 
-#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
-#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
-#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
-#define EACH(x, a) for (auto& x : a)
-
 const double EPS = 1e-9;
 const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
@@ -39,7 +28,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 
 	int tc; cin >> tc;
-	FOR(tt, 1, tc + 1) {
+	for (int tt = 1; tt <= tc; tt++) {
 		
 	}
 
