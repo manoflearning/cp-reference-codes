a00e94b77fcc0827018de8b52b1382d2460d0fde
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 16:44:52 +0900

    Change CCW, README.md

--- Reference Codes/6. Geometry/6.1. CCW Algorithm.cpp
+++ Reference Codes/6. Geometry/6.1. CCW Algorithm.cpp
@@ -1,24 +1,32 @@
+// Input: two line segments ab and cd
+// Output: 1 if ab and cd intersect, 0 if they do not intersect
+// BOJ 17387 AC Code
+// https://www.acmicpc.net/problem/17387
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define pii pair<int, int>
-struct point {
-	ll x, y;
-};
+struct point { ll x, y; };
 ll ccw(const point& a, const point& b, const point& c) {
-	// res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
-	ll res = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
-	return (res > 0 ? 1 : (res < 0 ? -1 : 0));
+    // res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
+    ll res = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
+    return (res > 0 ? 1 : (res < 0 ? -1 : 0));
 }
 // Does the line segment ab and cd intersect?
 bool isCross(point a, point b, point c, point d) {
-	ll ab = ccw(a, b, c) * ccw(a, b, d);
-	ll cd = ccw(c, d, a) * ccw(c, d, b);
-	if (ab == 0 && cd == 0) {
-		pii A = { a.x, a.y }, B = { b.x, b.y }, C = { c.x, c.y }, D = { d.x, d.y };
-		if (A > B) swap(A, B);
-		if (C > D) swap(C, D);
-		return (A <= D && C <= B);
-	}
-	else return (ab <= 0 && cd <= 0);
+    ll ab = ccw(a, b, c) * ccw(a, b, d);
+    ll cd = ccw(c, d, a) * ccw(c, d, b);
+    if (ab == 0 && cd == 0) {
+        pii A = { a.x, a.y }, B = { b.x, b.y }, C = { c.x, c.y }, D = { d.x, d.y };
+        if (A > B) swap(A, B);
+        if (C > D) swap(C, D);
+        return (A <= D && C <= B);
+    }
+    else return (ab <= 0 && cd <= 0);
+}
+int main() {
+    point a, b, c, d;
+    cin >> a.x >> a.y >> b.x >> b.y;
+    cin >> c.x >> c.y >> d.x >> d.y;
+    cout << isCross(a, b, c, d);
 }
