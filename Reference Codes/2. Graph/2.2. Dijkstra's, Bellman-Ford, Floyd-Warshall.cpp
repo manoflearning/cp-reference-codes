8fd3dc2096c956eabae3a9cf47dda1128de96b3a
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 18:16:26 +0900

    Change Dijkstra's ~

--- Reference Codes/2. Graph/2.2. Dijkstra's, Bellman-Ford, Floyd-Warshall.cpp
+++ Reference Codes/2. Graph/2.2. Dijkstra's, Bellman-Ford, Floyd-Warshall.cpp
@@ -1,23 +1,30 @@
 // Dijkstra's Algorithm
 
-// Time Complexity: O(ElogV)
+// INPUT: Given a directed graph with positive weights on the edges. Given a starting vertex.
+// OUTPUT: Outputs the shortest distance from the starting vertex to all vertices.
+// TIME COMPLEXITY: O(ElogV)
 
 // BOJ 1753 AC Code
 // https://www.acmicpc.net/problem/1753
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+
 const ll INF = 1e18;
 const int MAX = 202020;
+
 struct wv {
 	ll w; int v;
 	bool operator<(const wv& rhs) const {
 		return w > rhs.w;
 	}
 };
+
 int n, m, st;
 vector<wv> adj[MAX];
 vector<ll> dist(MAX, INF);
+
 void input() {
 	cin >> n >> m >> st;
 	for (int i = 0; i < m; i++) {
@@ -26,6 +33,7 @@ void input() {
 		adj[u].push_back({ w, v });
 	}
 }
+
 void dijkstra(int st) {
 	priority_queue<wv> pq;
 	pq.push({ 0, st });
@@ -42,31 +50,47 @@ void dijkstra(int st) {
 		}
 	}
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
 	input();
+
 	dijkstra(st);
+
 	for (int i = 1; i <= n; i++) {
 		if (dist[i] == INF) cout << "INF\n";
 		else cout << dist[i] << '\n';
 	}
+
 	return 0;
 }
+
 // Bellman-Ford Algorithm
+
+// INPUT: Given a directed gragh with weighted edges (possibly negative weight). Given a starting vertex.
+// OUTPUT: Outputs the shortest distance from the starting vertex to all vertices.
+// TIME COMPLEXITY: O(VE)
+
 // BOJ 11657 AC Code
 // https://www.acmicpc.net/problem/11657
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+
 const int MAX = 101010;
 const ll INF = 1e18;
+
 struct wv {
 	ll w; int v;
 };
+
 int n, m;
 vector<wv> adj[MAX];
 vector<ll> upper(MAX, INF);
+
 void input() {
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
@@ -75,6 +99,7 @@ void input() {
 		adj[u].push_back({ w, v });
 	}
 }
+
 int bellmanFord() {
 	upper[1] = 0;
 	int update = 1;
@@ -94,10 +119,12 @@ int bellmanFord() {
 	}
 	return !update;
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 	input();
+
 	if (bellmanFord()) {
 		for (int i = 2; i <= n; i++) {
 			if (upper[i] == INF) cout << -1 << '\n';
@@ -105,18 +132,29 @@ int main() {
 		}
 	}
 	else cout << -1 << '\n';
+
 	return 0;
 }
+
 // Floyd-Warshall Algorithm
+
+// INPUT: Given a directed graph with positive weights on the edges. Given a starting vertex.
+// OUTPUT: Outputs the shortest distance from all vertices to all vertices.
+// TIME COMPLEXITY: O(V^3)
+
 // BOJ 11404 AC Code
 // https://www.acmicpc.net/problem/11404
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+
 const ll INF = 1e18;
 const int MAXV = 101;
+
 int n, m;
 ll adj[MAXV][MAXV];
+
 void init() {
 	for (int i = 0; i < MAXV; i++) {
 		for (int j = 0; j < MAXV; j++) {
@@ -124,6 +162,7 @@ void init() {
 		}
 	}
 }
+
 void input() {
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
@@ -132,6 +171,7 @@ void input() {
 		adj[u][v] = min(adj[u][v], w);
 	}
 }
+
 void floyd() {
 	for (int i = 1; i <= n; i++) adj[i][i] = 0;
 	for (int k = 1; k <= n; k++) {
@@ -142,12 +182,17 @@ void floyd() {
 		}
 	}
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+	
 	init();
+	
 	input();
+	
 	floyd();
+	
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			if (adj[i][j] == INF) cout << 0 << ' ';
@@ -155,5 +200,6 @@ int main() {
 		}
 		cout << '\n';
 	}
+
 	return 0;
 }
