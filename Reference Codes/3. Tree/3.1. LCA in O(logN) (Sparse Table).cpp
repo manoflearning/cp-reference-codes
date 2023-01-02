71d784621efdebab9c19250af2929766979d6a48
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-03 05:56:16 +0900

    Change LCA

--- Reference Codes/3. Tree/3.1. LCA in O(logN) (Sparse Table).cpp
+++ Reference Codes/3. Tree/3.1. LCA in O(logN) (Sparse Table).cpp
@@ -1,10 +1,18 @@
+// INPUT: Given a tree with n vertices. Given a Q query (u, v).
+// OUTPUT: For each query (u, v), print the LCA of u and v.
+// TIME COMPLEXITY: O(nlogn) for build sparse table, O(logn) for each query.
+
 // BOJ 11438 AC Code
 // https://www.acmicpc.net/problem/11438
+
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MAX = 101010, MAXD = 16;  // 2^MAXD = 65536
+
 vector<int> adj[MAX];
 int n, dep[MAX], par[MAX][MAXD + 1];
+
 void input() {
 	cin >> n;
 	for (int i = 0; i < n - 1; i++) {
@@ -14,6 +22,7 @@ void input() {
 		adj[v].push_back(u);
 	}
 }
+
 void dfs(int now, int prv) {
 	par[now][0] = prv;
 	dep[now] = dep[prv] + 1;
@@ -22,6 +31,7 @@ void dfs(int now, int prv) {
 		dfs(i, now);
 	}
 }
+
 void buildSparseTable() {
 	for (int i = 1; i <= MAXD; i++) {
 		for (int v = 1; v <= n; v++) {
@@ -29,6 +39,7 @@ void buildSparseTable() {
 		}
 	}
 }
+
 int lca(int u, int v) {
 	if (dep[u] < dep[v]) swap(u, v);
 	int diff = dep[u] - dep[v];
@@ -43,17 +54,21 @@ int lca(int u, int v) {
 	}
 	return par[u][0];
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
 	input();
 	dfs(1, 0);
 	buildSparseTable();
+	
 	int Q; cin >> Q;
 	while (Q--) {
 		int u, v;
 		cin >> u >> v;
 		cout << lca(u, v) << '\n';
 	}
+
 	return 0;
 }
