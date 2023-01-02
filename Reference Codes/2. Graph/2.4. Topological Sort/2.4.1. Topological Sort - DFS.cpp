fe947c1f989d74856a75c2557c031c5fdc43a211
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-03 05:07:53 +0900

    Change Topological Sort

--- Reference Codes/2. Graph/2.4. Topological Sort/2.4.1. Topological Sort - DFS.cpp
+++ Reference Codes/2. Graph/2.4. Topological Sort/2.4.1. Topological Sort - DFS.cpp
@@ -1,12 +1,19 @@
+// INPUT: Given a directed graph.
+// OUTPUT: Output one of the topological sorting results of the graph.
+// TIME COMPLEXITY: O(n + m)
+
 // BOJ 2252 AC Code
 // https://www.acmicpc.net/problem/2252
+
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MAX = 101010;
+
 int n, m;
-vector<int> adj[MAX];
-stack<int> stk;
+vector<int> adj[MAX], stk;
 int vi[MAX], fi[MAX], isCycle;
+
 void input() {
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
@@ -15,6 +22,7 @@ void input() {
 		adj[u].push_back(v);
 	}
 }
+
 void dfs(int v) {
 	vi[v] = 1;
 	for (int next : adj[v]) {
@@ -22,24 +30,26 @@ void dfs(int v) {
 		else if (!fi[next]) isCycle = 1;
 	}
 	fi[v] = 1;
-	stk.push(v);
+	stk.push_back(v);
 }
+
 void topologicalSort() {
 	for (int i = 1; i <= n; i++) {
 		if (!vi[i]) dfs(i);
 	}
 	if (isCycle) cout << 0;
 	else {
-		while (!stk.empty()) {
-			cout << stk.top() << ' ';
-			stk.pop();
-		}
+		for (int i = stk.size() - 1; i >= 0; i--) 
+			cout << stk[i] << ' ';
 	}
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
 	input();
 	topologicalSort();
+
 	return 0;
 }
