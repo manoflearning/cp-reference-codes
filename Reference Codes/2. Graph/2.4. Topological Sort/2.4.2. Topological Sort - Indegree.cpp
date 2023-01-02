c7defbd305b1b6c80d02ba627e6f6db2a608447d
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-03 05:09:44 +0900

    Change Topological Sort

--- Reference Codes/2. Graph/2.4. Topological Sort/2.4.2. Topological Sort - Indegree.cpp
+++ Reference Codes/2. Graph/2.4. Topological Sort/2.4.2. Topological Sort - Indegree.cpp
@@ -1,13 +1,22 @@
+// INPUT: Given a directed graph.
+// OUTPUT: Output one of the topological sorting results of the graph.
+// TIME COMPLEXITY: O(n + m)
+
 // BOJ 2252 AC Code
 // https://www.acmicpc.net/problem/2252
+
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MAX = 101010;
+
 int n, m;
 vector<int> adj[MAX], ts;
 int ind[MAX], isCycle;
+
 void input() {
 	cin >> n >> m;
+
 	for (int i = 0; i < m; i++) {
 		int u, v;
 		cin >> u >> v;
@@ -15,10 +24,12 @@ void input() {
 		ind[v]++;
 	}
 }
+
 void topologicalSort() {
 	queue<int> q;
 	for (int i = 1; i <= n; i++)
 		if (ind[i] == 0) q.push(i);
+
 	for (int i = 0; i < n; i++) {
 		if (q.empty()) {
 			isCycle = 1;
@@ -32,16 +43,20 @@ void topologicalSort() {
 			if (ind[next] == 0) q.push(next);
 		}
 	}
+
 	if (isCycle) cout << 0;
 	else {
 		for (int i = 0; i < ts.size(); i++)
 			cout << ts[i] << ' ';
 	}
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
 	input();
 	topologicalSort();
+
 	return 0;
 }
