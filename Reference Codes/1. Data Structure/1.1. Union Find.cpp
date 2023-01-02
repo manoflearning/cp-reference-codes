213bedbb71ece6cbb39e522b9b67c3cf357af8ab
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 17:02:23 +0900

    Change Union Find

--- Reference Codes/1. Data Structure/1.1. Union Find.cpp
+++ Reference Codes/1. Data Structure/1.1. Union Find.cpp
@@ -1,24 +1,37 @@
+// Input: Given n+1 sets {0}, {1}, ..., {n}.
+// Two types of operations are given. 0 a b is the union operation. 1 a b is an operation that checks whether a and b are in the same set.
+// Output: For inputs starting with 1, the result is output as YES/NO.
+// Time Complexity: O(α(n)) (α is inverse Ackermann function)
+
 // BOJ 1717 AC Code
 // https://www.acmicpc.net/problem/1717
+
 #include <bits/stdc++.h>
 using namespace std;
+
 const int MAX = 1010101;
+
 int n;
 vector<int> uf(MAX, -1);
+
 int find(int x) {
 	if (uf[x] < 0) return x;
 	return uf[x] = find(uf[x]);
 }
+
 void merge(int a, int b) {
 	int A = find(a), B = find(b);
 	if (A == B) return;
 	uf[A] += uf[B];
 	uf[B] = A;
 }
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
 	cin >> n;
+
 	int q; cin >> q;
 	while (q--) {
 		int op, u, v;
@@ -29,5 +42,6 @@ int main() {
 			else cout << "NO\n";
 		}
 	}
+
 	return 0;
 }
