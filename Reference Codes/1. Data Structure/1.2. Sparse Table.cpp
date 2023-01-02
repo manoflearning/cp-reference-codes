e7edc1abbe33479f5f790f79ee8d0e600e459e8e
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 17:27:14 +0900

    Change Sparse Table

--- Reference Codes/1. Data Structure/1.2. Sparse Table.cpp
+++ Reference Codes/1. Data Structure/1.2. Sparse Table.cpp
@@ -1,28 +1,43 @@
+// INPUT: Given a function f: { 1, 2, ..., m } -> { 1, 2, ..., m }.
+// The function f is defined as f_1(x) = f(x), f_{n+1}(x) = f(f_n(x)).
+// OUTPUT: Given n and x, f_n(x).
+// TIME COMPLEXITY: O(mlogn) for build sparse table, O(logn) for each query.
+
+// BOJ 17435 AC Code
+// https://www.acmicpc.net/problem/17435
+
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 202020, MAXD = 17;
+const int MAX = 202020, MAXD = 18;
 
-int sp[MAX][MAXD];
+int m, sp[MAX][MAXD + 1];
 
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-  	// initialize
-	for (int i = 0; i < MAX; i++) cin >> sp[i][0];
-	for (int i = 0; i < MAX; i++) {
-		for (int j = 1; j < MAXD; j++) {
-			sp[i][j] = sp[sp[i][j - 1]][j - 1];
+
+	// input
+	cin >> m;
+	for (int i = 1; i <= m; i++) cin >> sp[i][0];
+
+	// build sparse table
+	for (int i = 1; i <= MAXD; i++) {
+		for (int j = 1; j <= m; j++) {
+			sp[j][i] = sp[sp[j][i - 1]][i - 1];
 		}
 	}
-  	// query
+
+	// query
 	int q; cin >> q;
-	for (int i = 0; i < q; i++) {
+	while (q--) {
 		int n, x;
 		cin >> n >> x;
-		for (int i = MAXD - 1; i >= 0; i--) {
-			if (n & sp[x][i]) x = sp[x][i];
+		for (int i = MAXD; i >= 0; i--) {
+			if (n & (1 << i)) x = sp[x][i];
 		}
+		cout << x << '\n';
 	}
+
 	return 0;
 }
