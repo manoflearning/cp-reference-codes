94d85276d523f7980f3c47679addd7a25ce70ebc
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 17:56:16 +0900

    Change Merge Sort Tree

--- Reference Codes/1. Data Structure/1.4. Merge Sort Tree/1.4.2. Iterative Merge Sort Tree.cpp
+++ Reference Codes/1. Data Structure/1.4. Merge Sort Tree/1.4.2. Iterative Merge Sort Tree.cpp
@@ -1,10 +1,17 @@
+// INPUT: Given a sequence A_1, A_2, ..., A_n of length n. Given a query (i, j, k).
+// OUTPUT: For each query (i, j, k), output the number of elements greater than k among elements A_i, A_{i+1}, ..., A_j.
+// TIME COMPLEXITY: O(nlogn) for initialize MST, O(log^2n) for each query.
+
 // BOJ 13537 AC Code
 // https://www.acmicpc.net/problem/13537
+
 #include <bits/stdc++.h>
 using namespace std;
 #define sz(x) (int)(x).size()
 #define all(c) (c).begin(), (c).end()
+
 const int MAX = 1 << 17;
+
 struct MergeSortTree {
     vector<int> t[MAX << 1];
     void build(const vector<int>& arr) {
@@ -26,15 +33,20 @@ struct MergeSortTree {
         return ret;
     }
 }mstree;
+
 int n;
 vector<int> arr;
+
 int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
+
     cin >> n;
     arr.resize(n);
     for (auto& i : arr) cin >> i;
+
     mstree.build(arr);
+
     int q; cin >> q;
     while (q--) {
         int a, b, c;
