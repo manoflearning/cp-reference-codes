490e5f2e58ecd30020f51e875f1f651884db2844
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-02 17:11:26 +0900

    Change Union Find

--- Reference Codes/1. Data Structure/1.2. Sparse Table.cpp
+++ Reference Codes/1. Data Structure/1.2. Sparse Table.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 1e5, MAXD = 17;
+
+const int MAX = 202020, MAXD = 17;
+
 int sp[MAX][MAXD];
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
