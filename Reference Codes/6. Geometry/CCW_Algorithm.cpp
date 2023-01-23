5fcc0dd93b1174df702ef27c604337bce47089eb
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-01-24 00:38:45 +0900

    Change CCW

--- Reference Codes/6. Geometry/CCW_Algorithm.cpp
+++ Reference Codes/6. Geometry/CCW_Algorithm.cpp
@@ -10,16 +10,16 @@ using namespace std;
 #define ll long long
 #define pii pair<int, int>
 
-struct point { ll x, y; };
+struct Point { ll x, y; };
 
-ll ccw(const point& a, const point& b, const point& c) {
+ll ccw(const Point& a, const Point& b, const Point& c) {
     // res > 0 -> ccw, res < 0 -> cw, res = 0 -> colinear
     ll res = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
     return (res > 0 ? 1 : (res < 0 ? -1 : 0));
 }
 
 // Does the line segment ab and cd intersect?
-bool isCross(point a, point b, point c, point d) {
+bool isCross(const Point& a, const Point& b, const Point& c, const Point& d) {
     ll ab = ccw(a, b, c) * ccw(a, b, d);
     ll cd = ccw(c, d, a) * ccw(c, d, b);
     if (ab == 0 && cd == 0) {
@@ -32,7 +32,7 @@ bool isCross(point a, point b, point c, point d) {
 }
 
 int main() {
-    point a, b, c, d;
+    Point a, b, c, d;
     cin >> a.x >> a.y >> b.x >> b.y;
     cin >> c.x >> c.y >> d.x >> d.y;
     cout << isCross(a, b, c, d);
