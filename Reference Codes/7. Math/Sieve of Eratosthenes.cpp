665a588572c841585606ee2cea9d0c78b9758740
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-05-01 10:03:09 +0900

    Update Sieve of Eratosthenes

--- Reference Codes/7. Math/Sieve of Eratosthenes.cpp
+++ Reference Codes/7. Math/Sieve of Eratosthenes.cpp
@@ -1,6 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
+// TIME COMPLEXITY: O(Nlog(log(N)))
 const int MAX = 1e6;
 bool isPrime[MAX + 1];
 vector<int> prime(1, 2);
@@ -8,7 +6,7 @@ void getPrime() {
 	fill(isPrime + 2, isPrime + MAX + 1, 1);
 	for (ll i = 4; i <= MAX; i += 2)
 		isPrime[i] = 0;
-	for (ll i = 3; i <= MAX; i++) {
+	for (ll i = 3; i <= MAX; i += 2) {
 		if (isPrime[i]) prime.push_back(i);
 		for (ll j = i * i; j <= MAX; j += i * 2)
 			isPrime[j] = 0;
