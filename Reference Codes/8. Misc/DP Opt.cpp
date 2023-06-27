e86639e515d63d22d3d1b7fda43869816fb61292
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-06-27 17:53:42 +0900

    Change DP Opt

--- Reference Codes/8. Misc/DP Opt.cpp
+++ Reference Codes/8. Misc/DP Opt.cpp
@@ -53,7 +53,6 @@ int main() {
     input();
     convexHullTrick();
     cout << dp[n];
-    return 0;
 }
 
 // Knuth Optimization
@@ -104,75 +103,30 @@ int main() {
         }
         cout << DP[1][n] << '\n';
     }
-    return 0;
 }
 
 // Divide and Conquer Optimization 
-// Recurrence: DP[t][i] = min(DP[t - 1][j] + C[j][i]) (1 <= j < n)
-// Condition: Let opt[t][i] be j with the smallest value of DP[t - 1][j] + C[j][i]. It must satisfy opt[t][i] <= opt[t][i + 1].
-// BOJ 12766 AC Code
-// https://www.acmicpc.net/problem/12766
+// Recurrence: dp[t][i] = min(dp[t - 1][j] + c[j][i]) (j < i)
+// Condition: Let opt[t][i] be j with the smallest value of dp[t - 1][j] + c[j][i]. It must satisfy opt[t][i] <= opt[t][i + 1].
+// Naive Complexity: O(m * n^2)
+// Optimized Complexity: O(m * nlogn)
+
+// BOJ 13261 AC Code
+// https://www.acmicpc.net/problem/13261
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 const ll INF = 1e18;
-const int MAX = 5050;
-int n, b, s, r;
-ll w[MAX], dp[MAX][MAX], psum[MAX];
-struct wv {
-    ll w; int v;
-    bool operator<(const wv& rhs) const {
-        return w > rhs.w;
-    }
-};
-vector<wv> adj[MAX], radj[MAX];
-void input() {
-    cin >> n >> b >> s >> r;
-    for (int i = 0; i < r; i++) {
-        int u, v, d;
-        cin >> u >> v >> d;
-        adj[u].push_back({ d, v });
-        radj[v].push_back({ d, u });
-    }
-}
-vector<ll> dist(MAX, INF), rdist(MAX, INF);
-void dijkstra() {
-    priority_queue<wv> pq;
-    pq.push({ 0, b + 1 });
-    dist[b + 1] = 0;
-    while (!pq.empty()) {
-        int v = pq.top().v;
-        ll w = pq.top().w;
-        pq.pop();
-        if (w > dist[v]) continue;
-        for (auto& i : adj[v]) {
-            if (dist[i.v] > w + i.w) {
-                dist[i.v] = w + i.w;
-                pq.push({ w + i.w, i.v });
-            }
-        }
-    }
-    pq.push({ 0, b + 1 });
-    rdist[b + 1] = 0;
-    while (!pq.empty()) {
-        int v = pq.top().v;
-        ll w = pq.top().w;
-        pq.pop();
-        if (w > rdist[v]) continue;
-        for (auto& i : radj[v]) {
-            if (rdist[i.v] > w + i.w) {
-                rdist[i.v] = w + i.w;
-                pq.push({ w + i.w, i.v });
-            }
-        }
-    }
-}
+int n, m;
+ll a[8080], psum[8080];
+ll dp[808][8080];
 void f(int gr, int l, int r, int nl, int nr) {
     int mid = (l + r) >> 1, idx = -1;
     ll& res = dp[gr][mid];
     res = INF;
     for (int i = nl; i <= min(mid, nr); i++) {
-        ll val = dp[gr - 1][i] + (mid - i - 1) * (psum[mid] - psum[i]);
+        assert(i <= mid);
+        ll val = dp[gr - 1][i] + (mid - i) * (psum[mid] - psum[i]);
         if (res > val) {
             res = val, idx = i;
         }
@@ -182,24 +136,26 @@ void f(int gr, int l, int r, int nl, int nr) {
         f(gr, mid + 1, r, idx, nr);
     }
 }
+
 int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    input();
-    dijkstra();
-    for (int i = 1; i <= b; i++) {
-        w[i] = dist[i] + rdist[i];
-    }
-    sort(w + 1, w + 1 + b);
-    for (int i = 1; i <= b; i++) {
-        psum[i] = w[i] + psum[i - 1];
-    }
-    for (int i = 1; i <= b; i++) {
-        dp[1][i] = (i - 1) * psum[i];
-    }
-    for (int i = 2; i <= s; i++) {
-        f(i, i, b, 0, b);
-    }
-    cout << dp[s][b];
-    return 0;
+
+    // input
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    // build prefix sum
+    for (int i = 1; i <= n; i++) 
+        psum[i] = a[i] + psum[i - 1];
+
+    // dp (dnc opt)
+    for (int i = 1; i <= n; i++)
+        dp[1][i] = i * psum[i];
+
+    for (int i = 2; i <= m; i++)
+        f(i, 0, n, 0, n);
+
+    // output
+    cout << dp[m][n];
 }
