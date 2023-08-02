db29b9f4ce607b81f8a9076c2c63c722842d8812
Jeong Woo-kyung <77jwk0724@gmail.com>
2023-08-03 04:27:14 +0900

    change C++ Basic Template.cpp

--- Reference Codes/0. Basic Template/C++ Basic Template.cpp
+++ Reference Codes/0. Basic Template/C++ Basic Template.cpp
@@ -22,11 +22,6 @@ const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
 const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
 int main() {
-	#ifndef ONLINE_JUDGE
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
-	freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
-	#endif
-
 	cin.tie(NULL); cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
