c3e1e9e4fb5b0d1f75a575d375f2756484de8fb7
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-10-15 16:03:33 +0900

    refactor: centroid decomposition

--- 3-tree/centroid_decomposition.cpp
+++ 3-tree/centroid_decomposition.cpp
@@ -1,10 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
+// TIME COMPLEXITY: O(n log n)
+// - The height of a centroid tree is O(log n).
+// - A path between two arbitrary nodes u, v in the original tree traverses l := lca(u, v) in the centroid tree. 
+//   We can think of the path (u, v) in the original tree as splitting into paths (u, l) and (l, v).
+// - In the original tree, all paths through (v, i) can be separated into paths through v, par(v), par(par(v)), ..., root in the centroid tree.
+namespace CD {
+const int MAXN = 202020;
 
-const int MAXV = 202020;
-
-vector<int> adj[MAXV];
-int used[MAXV], siz[MAXV], dep[MAXV];
+vector<int> adj[MAXN], cdchd[MAXN];
+int siz[MAXN], cdpar[MAXN];
+bool used[MAXN];
 
 int getSize(int now, int prv) {
   siz[now] = 1;
@@ -14,17 +18,20 @@ int getSize(int now, int prv) {
   }
   return siz[now];
 }
-int getCent(int now, int prv, int cnt) {
+int getCent(int now, int prv, int mxsiz) {
   for (auto &i : adj[now]) {
     if (used[i] || i == prv) continue;
-    if (siz[i] > cnt / 2) return getCent(i, now, cnt);
+    if (siz[i] > mxsiz / 2) return getCent(i, now, mxsiz);
   }
   return now;
 }
 void cd(int now, int prv) {
-  int cnt = getSize(now, prv);
-  int cent = getCent(now, prv, cnt);
+  int mxsiz = getSize(now, prv);
+  int cent = getCent(now, prv, mxsiz);
+  cdpar[cent] = prv;
+  cdchd[prv].push_back(cent);
   used[cent] = 1;
   for (auto i : adj[cent])
     if (!used[i]) cd(i, cent);
 }
+} // namespace CD
