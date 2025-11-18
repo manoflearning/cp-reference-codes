4722b443f2a8e23bbf70aa4c584f32c7317e1367
Wookyung Jeong <77jwk0724@gmail.com>
2025-11-18 21:37:33 +0900

    feat: add erasable priority queue (#22)

---
+++ refcode/1-ds/erasable_pq.hpp
@@ -0,0 +1,16 @@
+#include "../common/common.hpp"
+namespace refcode {
+template <class T, class O = less<T>>
+struct pq_set {
+  priority_queue<T, vector<T>, O> q, del;
+  const T &top() const { return q.top(); }
+  int size() const { return int(q.size() - del.size()); }
+  bool empty() const { return !size(); }
+  void insert(const T x) { q.push(x), flush(); }
+  void pop() { q.pop(), flush(); }
+  void erase(const T x) { del.push(x), flush(); }
+  void flush() {
+    while (del.size() && q.top() == del.top()) q.pop(), del.pop();
+  }
+};
+} // namespace refcode
