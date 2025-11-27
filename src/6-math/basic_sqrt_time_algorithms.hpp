1de6e6c814443e4fc2ebc4c9fe9f1d8085ee0fda
manoflearning <77jwk0724@gmail.com>
2025-11-28 00:18:38 +0900

    refactor: basic sqrt time algorithms

--- src/6-math/basic_sqrt_time_algorithms.hpp
+++ src/6-math/basic_sqrt_time_algorithms.hpp
@@ -1,35 +1,31 @@
 #include "../common/common.hpp"
-
 // 1. Finding Divisors in O(sqrt(x))
 // INPUT: Given a natural number x.
 // OUTPUT: Find all the divisors of x.
 // TIME COMPLEXITY: O(sqrt(x)).
-vector<int> di;
-void findingDivisors(int x) {
-  for (int i = 1; i <= sqrt(x); i++) {
+vector<ll> get_div(ll x) {
+  vector<ll> ret;
+  for (ll i = 1; i * i <= x; i++) {
     if (x % i == 0) {
-      di.push_back(i);
-      if (x / i != i) di.push_back(x / i);
+      ret.push_back(i);
+      if (x / i != i) ret.push_back(x / i);
     }
   }
-  sort(di.begin(), di.end());
+  sort(all(ret));
+  return ret;
 }
-
 // 2. Finding Prime Factorization in O(sqrt(x))
 // INPUT: Given a natural number x.
 // OUTPUT: Find the result of the prime factorization of x.
 // TIME COMPLEXITY: O(sqrt(x)).
-vector<int> p;
-void primeFactorization(int x) {
-  while (x % 2 == 0) {
-    x /= 2;
-    p.push_back(2);
-  }
+vector<ll> factorize(ll x) {
+  vector<ll> ret;
+  while (x % 2 == 0)
+    x /= 2, ret.push_back(2);
   for (int i = 3; i <= sqrt(x); i += 2) {
-    while (x % i == 0) {
-      x /= i;
-      p.push_back(i);
-    }
+    while (x % i == 0)
+      x /= i, ret.push_back(i);
   }
-  if (x > 1) p.push_back(x);
+  if (x > 1) ret.push_back(x);
+  return ret;
 }
