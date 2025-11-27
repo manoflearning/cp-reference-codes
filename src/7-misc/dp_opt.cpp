870cfff256d2b8f06a9696eec32a014685f96b8f
manoflearning <77jwk0724@gmail.com>
2025-11-27 22:59:33 +0900

    feat: add slope trick

--- src/7-misc/dp_opt.cpp
+++ src/7-misc/dp_opt.cpp
@@ -132,3 +132,24 @@ int main() {
   // output
   cout << dp[m][n];
 }
+
+// 4. Slope Trick
+// PROBLEM: You are given the array containing n integers.
+// At one turn you can pick any element and increase or decrease it by 1.
+// The goal is the make the array strictly increasing by making the minimum possible number of operations.
+// TIME COMPLEXITY: O(n log(n))
+ll slope_trick(vector<ll> a) {
+  ll ret = 0;
+  priority_queue<ll> pq;
+  for (int i = 0; i < sz(a); i++) {
+    a[i] -= i; // Change strictly increasing condition to non-decreasing condition
+    pq.push(a[i]);
+
+    if (pq.top() > a[i]) {
+      ret += pq.top() - a[i];
+      pq.pop();
+      pq.push(a[i]);
+    }
+  }
+  return ret;
+}
