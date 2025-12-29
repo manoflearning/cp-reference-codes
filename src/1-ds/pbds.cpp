5d314398b80b936ad31713dc8ed8663038dde315
manoflearning <77jwk0724@gmail.com>
2025-12-29 19:18:42 +0900

    feat: add multiset pbds

--- src/1-ds/pbds.cpp
+++ src/1-ds/pbds.cpp
@@ -2,7 +2,15 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-using ordered_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
-ordered_set st;
+using pbds = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+pbds st;
 st.order_of_key(x);  // number of elements strictly less than x
 st.find_by_order(x); // value of xth element (0-based)
+
+// multiset pbds (use "less_equal<int>" and custom "m_erase")
+using multi_pbds = tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>;
+void m_erase(multi_pbds &OS, int val){
+    int index = OS.order_of_key(val);
+    multi_pbds::iterator it = OS.find_by_order(index);
+    OS.erase(it);
+}
