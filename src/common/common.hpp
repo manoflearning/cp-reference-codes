42caa12dac997c03a70c26c3d3821b97e57ba339
manoflearning <77jwk0724@gmail.com>
2026-01-04 02:42:20 +0900

    remove

--- src/common/common.hpp
+++ src/common/common.hpp
@@ -4,7 +4,6 @@
 using namespace std;
 using ll = long long;
 using ld = long double;
-using ull = unsigned long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 #define fr first
@@ -12,4 +11,3 @@ using pll = pair<ll, ll>;
 #define pb push_back
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
